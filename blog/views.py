9bd7ae1bd53da4098f92d943cf613343a7ab097e
Aquakor <kornelux1@gmail.com>
2019-01-19 13:45:38 +0100

    Add PostUpdateView, PostDeleteView

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
 from .models import Post
 
@@ -11,14 +11,17 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+
 class PostDetailView(DetailView):
     model = Post
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
@@ -32,6 +35,40 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         # Run the parent class's form_valid
         # method and pass the updated argument.
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # Override form_valid method so we can add an author to the Post
+    # before commiting it to the database.
+    def form_valid(self, form):
+        # Take the instance and set the author to the current logged in user.
+        form.instance.author = self.request.user
+        # Run the parent class's form_valid
+        # method and pass the updated argument.
+        return super().form_valid(form)
+
+    def test_func(self):
+          post = self.get_object()
+          if self.request.user == post.author:
+              return True
+          else:
+              return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+          post = self.get_object()
+          if self.request.user == post.author:
+              return True
+          else:
+              return False
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
